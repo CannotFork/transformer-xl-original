b84ae29177b8dbc99ea96bae9a1c3611d4a5af7d
PeganovAnton <peganoff2@mail.ru>
2020-09-07 06:24:25 -0700

    Add ngc commands

---
+++ ngc_commands/original_ngc_command.sh
@@ -0,0 +1,15 @@
+cd transformer-xl-original \
+  && pip install -r requirements.txt \
+  && mkdir -p data \
+  && cd data \
+  && wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip \
+  && unzip -q wikitext-103-v1.zip \
+  && cd wikitext-103 \
+  && mv wiki.train.tokens train.txt \
+  && mv wiki.valid.tokens valid.txt \
+  && mv wiki.test.tokens test.txt \
+  && cd ../.. \
+  && echo 'Run training...' \
+  && cd pytorch \
+  && bash run_wt103_base.sh train  
+
