5cf04bad8e0a94b15f16714206105e22275f9709
PeganovAnton <peganoff2@mail.ru>
2020-09-07 07:58:00 -0700

    Increase number of visible gpus

--- ngc_commands/original_ngc_command.sh
+++ ngc_commands/original_ngc_command.sh
@@ -1,5 +1,5 @@
 cd transformer-xl-original \
-  && export CUDA_VISIBLE_DEVICES=0,1,2,3 \
+  && export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
   && mkdir -p data \
   && cd data \
   && wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip \
@@ -9,7 +9,6 @@ cd transformer-xl-original \
   && mv wiki.valid.tokens valid.txt \
   && mv wiki.test.tokens test.txt \
   && cd ../.. \
-  && echo 'Run training...' \
   && cd pytorch \
   && bash run_wt103_base_ngc.sh train  
 
